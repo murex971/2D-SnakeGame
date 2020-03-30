dd8207101caf19ee76280561af3051c81b1f2caf
leshnabalara <42586755+leshnabalara@users.noreply.github.com>
2020-03-31 01:34:39 +0530

    Update game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,25 +10,25 @@ describe Game do
     end
   end
 
-  it "#check_snake_position checks all checks successfully" do
+  it "#checkSnakePosition checks all checks successfully" do
     expect{new_game.checkSnakePosition}.to_not raise_error(AteItselfError)
     expect{new_game.checkSnakePosition}.to_not change{new_game.snake.body}
   end
 
-  it "#check_if_snake_ate_itself" do
+  it "#checkIfSnakeAteItself" do
     new_game.snake.parts[0] = new_game.snake.parts.last
     expect{new_game.checkIfSnakeAteItself}.to raise_error(AteItselfError)
     expect{new_game.checkSnakePosition}.to raise_error(AteItselfError)
   end
 
-  it "#check_if_snake_met_wall" do
+  it "#checkIfSnakeMetWall" do
     new_game.snake.parts[0][1] = new_game.gamefloor.width
     expect{new_game.checkIfSnakeMetWall}.to change{new_game.snake.parts[0][1]}.from(new_game.gamefloor.width).to(0)
     new_game.snake.parts[0][1] = new_game.gamefloor.width
     expect{new_game.checkSnakePosition}.to change{new_game.snake.parts[0][1]}.from(new_game.gamefloor.width).to(0)
   end
 
-  it "#check_if_snake_ate_point" do
+  it "#checkIfSnakeAtePoint" do
     new_game.snake.parts[0] = new_game.point.coordinates
     expect{new_game.checkIfSnakeAtePoint}.to change{new_game.snake.size}.from(4).to(5)
     new_game.snake.parts[0] = new_game.point.coordinates
@@ -41,11 +41,11 @@ describe Game do
     expect(new_game.compareKey(65, 'Q')).to be_falsey
   end
 
-  it "#execute_action quit on Q" do
+  it "#executeAction quit on Q" do
     expect(new_game.executeAction('q'.ord)).to eql(false)
   end
 
-  it "#execute_action turn on a" do
+  it "#executeAction turn on a" do
     expect{new_game.executeAction('d'.ord)}.to change{new_game.snake.direction}.from(:left).to(:right)
     expect(new_game.executeAction('d'.ord)).not_to be_nil
   end
